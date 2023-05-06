bc07915a4cd1a6d631730bc98181e3f215a5479b
Notistris <mizavayx@gmail.com>
2023-05-06 20:10:55 +0700

    testing

---
+++ config/databaseConn.js
@@ -0,0 +1,9 @@
+
+let Sequelize = require("sequelize");
+
+let sequelize = new Sequelize("database", "username", "password", {
+  host: "localhost",
+  dialect: "mysql"
+});
+
+module.exports = sequelize;
