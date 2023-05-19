aae2ca57ce49a2eee8dcdcfb38d6196b0a94184e
Notistris <73058358+mizavayx@users.noreply.github.com>
2023-05-19 03:55:28 +0000

    delete unused code

--- config/databaseConn.js
+++
@@ -1,9 +0,0 @@
-
-let Sequelize = require("sequelize");
-
-let sequelize = new Sequelize("database", "username", "password", {
-  host: "localhost",
-  dialect: "mysql"
-});
-
-module.exports = sequelize;
