0697e189184d5f8056add47caca0d8b642b37666
Notistris <73058358+mizavayx@users.noreply.github.com>
2023-05-26 11:21:32 +0000

    Delete files

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
-
-const dotenv = require("dotenv");
 const { connectDB } = require("./config/dbConnection");
+const { notFound, errorHandler } = require("./errorHandle");
+const dotenv = require("dotenv");
 const cors = require("cors");
-
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 connectDB();
+
+// Routers
+
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`App is listening port ${PORT}`);
